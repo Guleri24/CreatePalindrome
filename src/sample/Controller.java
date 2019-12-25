7b66bf31eeb793d7b81786faf75367f8f3378654
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-12-25 22:08:04 +0530

    condition added
    check condition for string contains the char frequency 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,11 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.collections.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import org.w3c.dom.ls.LSOutput;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Controller {
 
@@ -18,26 +13,46 @@ public class Controller {
     public ListView<String> palindromeList;
     public TextField enter;
 
-    public void onClickSubmit(ActionEvent actionEvent) {
+    public void onClickSubmit() {
         submit.setOnAction(e -> {
             String string = enter.getText();
-            palindromeList.setItems(createPalindrome(string));
+            palindromeList.setItems(generatePalindrome(string));
         });
     }
 
-    private ObservableList<String> createPalindrome(String string) {
-        char[] chars = string.toCharArray();
+    private ObservableList<String> generatePalindrome(String s) {
+        char[] chars = s.toCharArray();
+        char temp;
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < chars.length; i++) {
-            for (int j = i; j < chars.length-1; j++) {
-               char[] s = Arrays.copyOfRange(chars,j,chars.length-1);
-               list.add(Arrays.toString(s));
+        if (chars.length < 2) list.add(s + s);
+        else {
+            for (int i = 0; i < chars.length / 2; i++) {
+                temp = chars[i];
+                chars[i] = chars[chars.length - i - 1];
+                chars[chars.length - i - 1] = temp;
             }
+            Map<Character, Integer> map = new HashMap<>();
+            // Traverse through array elements and count frequencies
+
+            for (char aChar : chars) {
+                if (map.containsKey(aChar)) map.put(aChar, map.get(aChar) + 1);
+                else map.put(aChar, 1);
+            }
+            if (map.containsValue(1)) {
+                list.add(nonMatchPalindrome(s,chars));
+            }
+            else {
+
+
+            }
+
         }
-        return FXCollections.observableArrayList(list);
+        return FXCollections.observableList(list);
+
     }
 
-    private void generatePalindrome(String string) {
-
+    private String nonMatchPalindrome(String s, char[] chars) {
+        return s+ String.valueOf(chars);
     }
 }
+

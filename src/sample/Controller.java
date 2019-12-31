45d65d687005f62e3c5540c374ba1056eebe6d6b
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-12-31 19:19:56 +0530

    Main computation shift to PalindromeGenerate.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,8 @@
 package sample;
 
-import javafx.collections.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
-import java.util.*;
 
 public class Controller {
 
@@ -16,43 +13,9 @@ public class Controller {
     public void onClickSubmit() {
         submit.setOnAction(e -> {
             String string = enter.getText();
-            palindromeList.setItems(generatePalindrome(string));
+            palindromeList.setItems(PalindromeGenerator.generatePalindrome(string));
         });
     }
-
-    private ObservableList<String> generatePalindrome(String s) {
-        char[] chars = s.toCharArray();
-        char temp;
-        List<String> list = new ArrayList<>();
-        if (chars.length < 2) list.add(s + s);
-        else {
-            for (int i = 0; i < chars.length / 2; i++) {
-                temp = chars[i];
-                chars[i] = chars[chars.length - i - 1];
-                chars[chars.length - i - 1] = temp;
-            }
-            Map<Character, Integer> map = new HashMap<>();
-            // Traverse through array elements and count frequencies
-
-            for (char aChar : chars) {
-                if (map.containsKey(aChar)) map.put(aChar, map.get(aChar) + 1);
-                else map.put(aChar, 1);
-            }
-            if (map.containsValue(1)) {
-                list.add(nonMatchPalindrome(s,chars));
-            }
-            else {
-
-
-            }
-
-        }
-        return FXCollections.observableList(list);
-
-    }
-
-    private String nonMatchPalindrome(String s, char[] chars) {
-        return s+ String.valueOf(chars);
-    }
 }
+
 

2dfb102e08bda42575b1d45f296bf4526baf19de
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-12-31 19:18:46 +0530

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,21 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.util.Arrays;
+import org.junit.Test;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Test
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Palindrome Generator");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
